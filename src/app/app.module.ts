e788f1c41e4bd53bd9343828f2dfc258a1bea28e
Gaetan Roger <gaetanroger.fr@gmail.com>
2018-03-09 17:24:33 +0100

    Add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import {AppComponent} from './app.component';
 import {TexDirective} from './directives/tex/tex.directive';
 import {LagrangeService} from './services/lagrange/lagrange.service';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule} from '@angular/material';
+import {MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatToolbarModule} from '@angular/material';
 import {FormsModule} from '@angular/forms';
+import {TopbarComponent} from './components/topbar/topbar.component';
 
 
 @NgModule({
     declarations: [
         AppComponent,
         TexDirective,
+        TopbarComponent,
     ],
     imports: [
         BrowserModule,
@@ -22,7 +24,8 @@ import {FormsModule} from '@angular/forms';
         MatButtonModule,
         MatFormFieldModule,
         MatInputModule,
-        MatIconModule
+        MatIconModule,
+        MatToolbarModule
     ],
     providers: [LagrangeService],
     bootstrap: [AppComponent]
